ae165eec108d9db35145cd742cc707bc06cd8151
Rafael Rossatto <rafarossatto25@gmail.com>
2026-03-08 10:15:27 -0300

    Make the implementation of the method of the temp_obs class.

--- c++/Np=free*0.25/L_128/Nc=0.5/src/temp_obs.cpp
+++ c++/Np=free*0.25/L_128/Nc=0.5/src/temp_obs.cpp
@@ -1,36 +1,26 @@
-#ifndef TEMP_OBS_H
-#define TEMP_OBS_H
-
-#include "obstacle.h"
-#include <string>
+#include "temp_obs.h"
 
 /**
- * @brief Classe que herda de Obstacle e adiciona atributo life
+ * @brief Construtor da classe temp_obs
+ * Inicializa a parte Obstacle e define m_life como 0
  */
-class temp_obs : public Obstacle {
-public:
-    /**
-     * @brief Construtor da classe temp_obs
-     * @param type Tipo do obstáculo
-     * @param id Identificador único
-     * @param positionX Posição X
-     * @param positionY Posição Y
-     */
-    temp_obs(const std::string& type, int id, int positionX, int positionY);
-    
-    /**
-     * @brief Soma uma unidade ao life
-     */
-    void addLife();
-    
-    /**
-     * @brief Retorna o valor atual do life
-     * @return int Valor do life
-     */
-    int getLife() const;
+temp_obs::temp_obs(const std::string& type, int id, int positionX, int positionY)
+    : Obstacle(type, id, positionX, positionY), m_life(0)
+{}
 
-private:
-    int m_life;  // Atributo life sempre começa com 0
-};
+/**
+ * @brief Soma uma unidade ao life
+ */
+void temp_obs::addLife()
+{
+    m_life++;
+}
 
-#endif // TEMP_OBS_H
+/**
+ * @brief Retorna o valor atual do life
+ * @return int Valor do life
+ */
+int temp_obs::getLife() const
+{
+    return m_life;
+}
